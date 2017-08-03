5b47966f526906c925f5d34d3feb274c8aac3d24
jamesKe <iamcdxn@gmail.com>
2017-08-03 09:42:14 +0800

    Install SwiftLint

---
+++ SearchPractice/Pods/Target Support Files/Pods-SearchPractice/Pods-SearchPractice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SearchPractice : NSObject
+@end
+@implementation PodsDummy_Pods_SearchPractice
+@end
---
+++ SearchPractice/Pods/Target Support Files/Pods-SearchPracticeUITests/Pods-SearchPracticeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SearchPracticeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SearchPracticeUITests
+@end
